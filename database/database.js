02533acf9637a2773c2353e2c6266aadf96a2520
Elijah Bobzom <ebobzom@gmail.com>
2020-05-05 07:57:52 +0100

    add heroku connection url

--- database/database.js
+++ database/database.js
@@ -1,12 +1,7 @@
 import mysql from 'mysql';
 import configObject from '../config/config';
 
-const connection = mysql.createConnection({
-    host: configObject.HOST,
-    user: configObject.USER,
-    password: configObject.PASSWORD,
-    database: configObject.DATABASE
-});
+const connection = mysql.createConnection('mysql://b0a24ffb970b98:05b8b738@us-cdbr-east-06.cleardb.net/heroku_4930a6e8618811b?reconnect=true');
 connection.connect((err) => {
     if(err){
         console.log('error connecting to db', err);
